9e84ca4e217a0792469b93037995ce80bb8e2cc5
racoonamatatas <emacanghaill@gmail.com>
2026-01-13 12:18:00 +0100

    define parent component with v-model ChildPart2 component

---
+++ src/components/ParentPart2.vue
@@ -0,0 +1,11 @@
+<template>
+    <ChildPart2 v-model="sharedMessage" />
+    <p>Current Message: {{ sharedMessage }}</p>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import ChildPart2 from './ChildPart2.vue';
+
+const sharedMessage = ref('');
+</script>
