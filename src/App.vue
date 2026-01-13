b3533645a5c174bb3bdccb595c6c9daa86703512
racoonamatatas <emacanghaill@gmail.com>
2026-01-13 12:18:36 +0100

    instantiate a ParentPart2 component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
 import ParentComponent from './components/ParentComponent.vue';
+import ParentPart2 from './components/ParentPart2.vue';
 </script>
 
 <template>
-    <ParentComponent />
+    <h1>Working with Components</h1>
+    <br />
+
+    <h2>Part1: defineProps and defineEmit</h2>
+    <div>
+        <ParentComponent />
+    </div>
+
+    <br />
+
+    <h2>Part2: defineModel</h2>
+    <div>
+        <ParentPart2 />
+    </div>
 </template>
